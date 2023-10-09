4a4f108d89a11908ddbbeadb63ad8c48f9a2bbae
sarajadidi <sararivkajadidi@gmail.com>
2023-10-09 09:08:14 -0700

    Completed Exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car() //- a special member method that has the same name as its class
+		{
+
+		}
+
+		public Car(string make, string model, int year)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+
+		
+		
+
+        //Create a Make property of type string that is public( and model, and year)
+
+		public string Make { get; set;  } //-read;write;
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car(); // "Car();" is a constructor -helps us create a new instance of the car class
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2021);
+
+            var carList = new List<Car>() { myCar, mazda, chevy};
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+            
         }
     }
 }
